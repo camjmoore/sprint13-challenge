cdeda3e1232e382813b6ad9535e0385a8e018aaf
camjmoore <cjmooredev@gmail.com>
2023-12-07 20:41:10 -0400

    remove requires from inside sever use calls

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const server = express();
-
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
 // Configure your server here
 server.use(express.json());
 // Build your actions router in /api/actions/actions-router.js
-server.use("/api/actions", require("./actions/actions-router"));
+server.use("/api/actions", actionsRouter);
 // Build your projects router in /api/projects/projects-router.js
-server.use("/api/projects", require("./projects/projects-router"));
+server.use("/api/projects", projectsRouter);
 
 module.exports = server;
