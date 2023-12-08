3794fc1d0493ebc7d0524cbfdb97bb95c2342b3c
camjmoore <cjmooredev@gmail.com>
2023-12-07 21:41:20 -0400

    implemented get endpoints

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,38 @@
 // Write your "projects" router here!
+const express = require("express");
+
+const Projects = require("./projects-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((projects) => {
+      if (projects) {
+        res.status(200).json(projects);
+      } else {
+        res.status(404).json([]);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Projects.get(req.params.id)
+    .then((project) => {
+      if (project) {
+        res
+          .status(200)
+          .json(project.find((project) => project.id == req.params.id));
+      } else {
+        res.status(404).json({});
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+module.exports = router;
