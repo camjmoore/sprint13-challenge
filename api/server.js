e38eb68d28f058adec299d8a4995b59e83f5aa1d
camjmoore <cjmooredev@gmail.com>
2023-12-07 20:26:42 -0400

    configured server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
 // Configure your server here
+server.use(express.json());
 // Build your actions router in /api/actions/actions-router.js
+server.use("/api/actions", require("./actions/actions-router"));
 // Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use("/api/projects", require("./projects/projects-router"));
 
 module.exports = server;
