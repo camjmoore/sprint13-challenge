5f42ea3c0f4a3cf6d417d1e8b0ea1a2f4057e650
camjmoore <cjmooredev@gmail.com>
2023-12-09 00:14:18 -0400

    implemented actions get and get by id endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,33 @@
 // Write your "actions" router here!
+
+const express = require("express");
+
+const Actions = require("./actions-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Actions.get(req.params.id)
+    .then((action) => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json([]);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+module.exports = router;
