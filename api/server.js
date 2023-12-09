bcc075b91b54e0dce933e92bf4f6ea68f6ad1f77
camjmoore <cjmooredev@gmail.com>
2023-12-09 00:13:45 -0400

    enable actions router

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const projectsRouter = require("./projects/projects-router");
 server.use(express.json());
 
 // Build your actions router in /api/actions/actions-router.js
-// server.use("/api/actions", actionsRouter);
+server.use("/api/actions", actionsRouter);
 
 // Build your projects router in /api/projects/projects-router.js
 server.use("/api/projects", projectsRouter);
