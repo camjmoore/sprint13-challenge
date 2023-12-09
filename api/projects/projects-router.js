8379dbebbaf41f467bcee0dbd8055664fcb8d2bb
camjmoore <cjmooredev@gmail.com>
2023-12-08 23:48:35 -0400

    implemented projects delete and get actions

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -75,4 +75,32 @@ router.put("/:id", (req, res) => {
     });
 });
 
+router.delete("/:id", (req, res) => {
+  Projects.remove(req.params.id)
+    .then((project) => {
+      if (project) {
+        res.status(200);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.get("/:id/actions", (req, res) => {
+  Projects.getProjectActions(req.params.id)
+    .then((actions) => {
+      if (actions) {
+        res.status(200).json(actions);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
 module.exports = router;
