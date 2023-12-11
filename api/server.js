d7228e340e33e88f1153616c57534554c189bd18
camjmoore <cjmooredev@gmail.com>
2023-12-10 21:47:32 -0400

    introduced helmet middleware

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
 const server = express();
 const actionsRouter = require("./actions/actions-router");
 const projectsRouter = require("./projects/projects-router");
 
 // Configure your server here
 server.use(express.json());
+server.use(helmet());
 
 // Build your actions router in /api/actions/actions-router.js
 server.use("/api/actions", actionsRouter);
